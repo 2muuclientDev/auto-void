4fa483a37f0f1a43698dee4f15b476f58ef7cbdc
unsnipeable <mtnkylolomg@gmail.com>
2025-07-13 18:42:23 +0900

    リファクタリング

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -15,11 +15,9 @@ public class ExampleMod {
     public static final String MODID = "autovoid";
     public static final String VERSION = "1.0";
 
-    public static boolean maphider = true;
-
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        System.out.println("2muu OP");
+        System.out.println("2muu OP - autovoiding! help!");
     }
 
 
