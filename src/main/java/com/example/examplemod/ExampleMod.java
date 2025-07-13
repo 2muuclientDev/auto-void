be821c3a5ee33c93ac84d72f24c98167aea89b03
unsnipeable <mtnkylolomg@gmail.com>
2025-07-13 18:36:18 +0900

    Initial commit

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,53 @@
+package com.example.examplemod;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+public class ExampleMod {
+    public static final String MODID = "autovoid";
+    public static final String VERSION = "1.0";
+
+    public static boolean maphider = true;
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println("2muu OP");
+    }
+
+
+    @SubscribeEvent
+    public void onRenderTick(TickEvent.RenderTickEvent event) {
+        if (event.phase != TickEvent.Phase.END) return;
+
+        Minecraft mc = Minecraft.getMinecraft();
+        GuiScreen current = mc.currentScreen;
+
+        if (current != null) {
+            if (current.getClass().getSimpleName().toLowerCase().contains("chat")) {
+                render();
+            }
+        } else {
+            render();
+        }
+    }
+
+    public void render() {
+        Minecraft mc = Minecraft.getMinecraft();
+        FontRenderer fr = mc.fontRendererObj;
+        ScaledResolution sr = new ScaledResolution(mc);
+
+        String text = "Auto Void §7Hypixel";
+        int x = sr.getScaledWidth() - fr.getStringWidth(text) - 5;
+        int y = 5;
+
+        fr.drawString(text, x, y, 0x19bfe0, true);
+    }
+}
