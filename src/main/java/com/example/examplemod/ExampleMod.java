efb1800b24dcb055ab8deca4342fd9823745cec8
unsnipeable <mtnkylolomg@gmail.com>
2025-07-21 07:36:22 +0900

    for auto flag collect

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -15,6 +15,9 @@ public class ExampleMod {
     public static final String MODID = "autovoid";
     public static final String VERSION = "1.0";
 
+    @Mod.Instance
+    public static ExampleMod instance;
+
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
